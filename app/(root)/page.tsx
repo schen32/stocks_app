cc61590517977f0263f11443379384b8a73e8fba
Simon Chen <simonc8192@gmail.com>
2026-01-14 06:08:16 -0800

    - trading view widget component and hook

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
 import { Button } from "@/components/ui/button";
+import { MARKET_DATA_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-      <Button>Click Me!</Button>
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_DATA_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          ></TradingViewWidget>
+        </div>
+      </section>
     </div>
   );
 };
