5e275004d5b76133e5bff3b8dbe7bc02b0c0bc1c
Simon Chen <simonc8192@gmail.com>
2026-01-14 04:31:02 -0800

    - shadcn avatar on user dropwdown

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -8,8 +8,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
 
 const UserDropdown = () => {
   const router = useRouter();
@@ -18,9 +20,23 @@ const UserDropdown = () => {
     router.push("/sign-in");
   };
 
+  const user = { name: "John", email: "contact@email.com" };
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-small font-bold">
+              {user.name[0]}
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
