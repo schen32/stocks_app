d1b87720fe9c5be41e7a0f22787135bed954bf8f
Simon Chen <simonc8192@gmail.com>
2026-01-15 08:57:21 -0800

    - sign in footer link

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
 import InputField from "@/components/forms/InputField";
 import SelectField from "@/components/forms/SelectField";
 import { Button } from "@/components/ui/button";
@@ -115,6 +116,11 @@ const SignUp = () => {
         >
           {isSubmitting ? "Creating account" : "Start Your Investing Journey"}
         </Button>
+        <FooterLink
+          text="Already have an account?"
+          linkText="Sign in"
+          href="/sign-in"
+        ></FooterLink>
       </form>
     </>
   );
